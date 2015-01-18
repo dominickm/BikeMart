7b262994bfc846c4eb849929200e4a2273cb9dac
Michael Dominick <michael@fingertiptech.com>
2015-01-17 20:59:09 -0500

    added Bike model

---
+++ BikeMart/Bike.h
@@ -0,0 +1,19 @@
+//
+//  Bike.h
+//  BikeMart
+//
+//  Created by michael on 1/17/15.
+//  Copyright (c) 2015 Fingertip Tech, INC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Bike : NSObject
+
+@property (nonatomic) NSString* name;
+@property (nonatomic) NSString* blurb;
+@property (nonatomic) NSNumber* price;
+@property (nonatomic) UIImage* image;
+
+@end
---
+++ BikeMart/Bike.m
@@ -0,0 +1,13 @@
+//
+//  Bike.m
+//  BikeMart
+//
+//  Created by michael on 1/17/15.
+//  Copyright (c) 2015 Fingertip Tech, INC. All rights reserved.
+//
+
+#import "Bike.h"
+
+@implementation Bike
+
+@end
